fa08e9082d1a9d4091bff8e6da22446ec722eb67
Maris Ruskulis <maris@co.inbox.lv>
2015-03-04 10:57:05 +0200

    remove static metrics

--- src/main/scala/com/kenshoo/play/metrics/MetricsFilter.scala
+++ src/main/scala/com/kenshoo/play/metrics/MetricsFilter.scala
@@ -26,39 +26,14 @@ trait MetricsFilter extends EssentialFilter {
 
   def registry: MetricRegistry
 
-  val knownStatuses = Seq(Status.OK, Status.BAD_REQUEST, Status.FORBIDDEN, Status.NOT_FOUND,
-    Status.CREATED, Status.TEMPORARY_REDIRECT, Status.INTERNAL_SERVER_ERROR)
-
-  def statusCodes: Map[Int, Meter] = knownStatuses.map(s => s -> registry.meter(name(classOf[MetricsFilter], s.toString))).toMap
-
-  def requestsTimer: Timer = registry.timer(name(classOf[MetricsFilter], "requestTimer"))
-
   def activeRequests: Counter = registry.counter(name(classOf[MetricsFilter], "activeRequests"))
-
-  def otherStatuses: Meter = registry.meter(name(classOf[MetricsFilter], "other"))
-
-  def staticMetrics(next: EssentialAction) = new EssentialAction {
-    def apply(rh: RequestHeader) = {
-      val context = requestsTimer.time()
-
-      def logCompleted(result: Result): Result = {
-        activeRequests.dec()
-        context.stop()
-        statusCodes.getOrElse(result.header.status, otherStatuses).mark()
-        result
-      }
-
-      activeRequests.inc()
-      next(rh).map(logCompleted)
-    }
-  }
 
   def apply(next: EssentialAction) = new EssentialAction {
 
     def apply(rh: RequestHeader) = {
 
       val method = rh.tags.getOrElse(play.api.Routes.ROUTE_ACTION_METHOD, "MetricsFilter")
-      val controller = rh.tags.getOrElse(play.api.Routes.ROUTE_CONTROLLER, "com.kenshoo.play.metrics")
+      val controller = rh.tags.getOrElse(play.api.Routes.ROUTE_CONTROLLER, getClass.getPackage.getName)
 
       val context = registry.timer(s"latency.$controller.$method").time()
 
